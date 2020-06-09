6189a5aa1c2eb10df4aeac51109af939a5d1fc12
Lizeth <61445557+LizzyO@users.noreply.github.com>
2020-06-08 21:06:05 -0700

    started level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,3 @@
-//create variable for data.js
 var sightings = data;
 
 // Get a reference to the table body
@@ -7,7 +6,7 @@ var tbody = d3.select("tbody");
 // Console.log the data from data.js
 console.log(data);
 
-//Use d3 to append one table row `tr` for each UFO Sighting object in the data.js
+//Use d3 to append one table row `tr` for each UFO Sighting object
 sightings.forEach(function(ufoSighting) {
     console.log(ufoSighting);
     var row = tbody.append("tr");
@@ -17,24 +16,27 @@ sightings.forEach(function(ufoSighting) {
     })
   });
 
+// Assign the data from `data.js` to a descriptive variable
+
 // Select the button
 var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select("form");
+var form = d3.select("#form");
 
-// Create event handlers for both enter and click
+// Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-// Event handler function for the form
+// Complete the event handler function for the form
 function runEnter() {
-
+console.log("test");
 // Prevent the page from refreshing
 d3.event.preventDefault();
 
 // Select the input element and get the raw HTML node
 var inputDate = d3.select("#datetime");
+var inputCity = d3.select("#city");
 
 // Get the value property of the input element
 var inputDateValue = inputDate.property("value");
@@ -42,15 +44,19 @@ var inputDateValue = inputDate.property("value");
 console.log(inputDateValue);
 console.log(sightings);
 
-// Use the form input to filter the data by datetime
+// Use the form input to filter the data by blood type
 var filteredDate = sightings.filter(ufosightings => ufosightings.datetime === inputDateValue);
+// var filteredCity = sightings.filter(ufosightings => ufosightings.city === inputValue);
 
 console.log(filteredDate);
 
-//select all the row that were created and remove
 d3.selectAll("tr").remove();
 
-//loop through new filtered data 
+
+// if filteredDate >== 1
+// then 
+
+
 filteredDate.forEach(function(ufoSighting) {
   console.log(ufoSighting);
   var row = tbody.append("tr");
